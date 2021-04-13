623c0664fa5f2b8936cb5a175db4b1af5f4df39b
itstoasted-0 <snow.w686@gmail.com>
2021-04-13 11:12:14 +0900

    seed.rbにテキスト教材のcsvデータを読み込む設定を記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+Text.delete_all
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |text|
+  Text.create!(text.to_h)
+end
+puts "テキスト教材インポート完了"
