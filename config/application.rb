ec0d9551e0dc43d6c5f9cd8f98cfa7304dcc6280
itstoasted-0 <snow.w686@gmail.com>
2021-04-14 13:07:58 +0900

    csvクラスを読み込む設定

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module TeamProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # lib/autoloads ディレクトリ配下のファイルを読み込む
+    config.autoload_paths << Rails.root.join("lib/autoloads")
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
