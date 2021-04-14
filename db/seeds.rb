01c928915a64c9f2c7e78554cb85268366683de3
itstoasted-0 <snow.w686@gmail.com>
2021-04-14 13:31:07 +0900

    import_csv.rb　に　require 'CSV'を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "csv"
-
-Text.delete_all
-CSV.foreach("db/csv_data/text_data.csv", headers: true) do |text|
-  Text.create!(text.to_h)
-end
-puts "テキスト教材インポート完了"
+Text.destroy_all
+ImportCsv.text_data
--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class ImportCsv
   def self.import(path)
     list = []
