c41f2e6f071dad9fcf61a6974458271895fa3c9a
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-04-24 01:04:25 +0000

    adminとuserのサインインを切り分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate!
+  
+    def authenticate!
+      if admin_user_signed_in?
+        authenticate_admin_user!
+    else
+        authenticate_user!
+    end
+  end
 end
