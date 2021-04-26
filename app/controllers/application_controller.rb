9238fefdf0b9aa1e5db4138d90eafefcea395512
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-04-26 07:09:48 +0000

    ユーザーのサインイン無しで管理者画面にログインできる実装を変更しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate!
-  
-  def authenticate!
-    if admin_user_signed_in?
-      authenticate_admin_user!
-    else
-      authenticate_user!
-    end
-  end
+  before_action :authenticate_user!
 end
