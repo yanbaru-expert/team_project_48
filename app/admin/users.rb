9ad39b1b11504e9df1713f1c06dc7fcf7393a2c9
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-04-26 13:41:03 +0000

    管理者画面でユーザーの作成ができるようにapp/admin/user.rbを修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register User do
-  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :email, :password, :password_confirmation
   
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
 end
