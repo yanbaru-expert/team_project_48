9827c2e8adc48b7c376c2fb2366cef1356c7cf5b
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-04-26 06:45:47 +0000

    app/admin/movies.rbのtitleのスペルミス修正,app/admin/users.rbのpermit_params修正

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Movie do
-  permit_params :genre, :titlw, :url
+  permit_params :genre, :title, :url
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :name, :age, :address
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
