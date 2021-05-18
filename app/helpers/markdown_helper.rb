da62674868a55052e82b119f8bfe0b665775a54b
mihomatsui <mihomatsui.24@gmail.com>
2021-05-18 19:27:47 +0900

    markdownヘルパーの実装

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,2 +1,26 @@
 module MarkdownHelper
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(html_renderer, markdown_extensions)
+    markdown.render(text).html_safe
+  end
+
+  private
+
+  def html_renderer
+    Redcarpet::Render::HTML
+  end
+
+  def markdown_extensions
+    {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      fanced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+    }
+  end
 end
