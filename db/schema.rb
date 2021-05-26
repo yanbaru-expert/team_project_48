41f205eff52affbfa71c13d8ac63ed125497a2a6
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-05-26 15:33:56 +0000

    userとwatchモデルの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_144049) do
+ActiveRecord::Schema.define(version: 2021_05_26_151149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,4 +69,12 @@ ActiveRecord::Schema.define(version: 2021_04_21_144049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "watches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_watches_on_user_id"
+  end
+
+  add_foreign_key "watches", "users"
 end
