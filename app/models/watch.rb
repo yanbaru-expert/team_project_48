06c54d0fb7b059fa1500ab2c32a5d0daca3059c9
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-05-30 15:27:45 +0000

    視聴済み機能のボタン作成

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,9 +1,4 @@
 class Watch < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  
-  # validates :user_id, uniquieness: {
-  #                       scope: :movie_id,
-  #                       message: "は同じ動画に2回以上視聴済みはできません",
-  #                     }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,6 @@ user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-movie1 = user2.movies.create!(url: "https://www.youtube.com/embed/iFMNUM2Q9mg")
-movie2 = user1.movies.create!(url: "https://www.youtube.com/embed/x_KeaEUr3jo")
-movie3 = user3.movies.create!(url: "https://www.youtube.com/embed/Outcx4StSyU")
-
-movie1.watches.create!(user_id: user1.id)
-movie2.watches.create!(user_id: user3.id)
-movie3.watches.create!(user_id: user2.id)
-
-
 AdminUser.find_or_create_by!(email: 'admin@example.com') do |admin_user|
   admin_user.password = PASSWORD
   puts "管理者ユーザーの初期データインポートに成功しました。"
