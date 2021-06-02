701987514e3c6b95cbe98b025bf0f4b633d92596
mihomatsui <mihomatsui.24@gmail.com>
2021-06-02 14:46:28 +0900

    ページ移動に対応したLv番号が表示されるよう実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
-  PER = 10
+  #1ページに表示される動画教材を定義
+  PER = 8
 
   def index
-    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(PER)
+    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page])
+    page_num = @movies.current_page
+    @base_level = (page_num - 1) * PER
   end
 end
