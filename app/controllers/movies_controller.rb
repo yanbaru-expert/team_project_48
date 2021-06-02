3b6cd34b808835f972000ae6cda006bd01898ef8
mihomatsui <mihomatsui.24@gmail.com>
2021-06-02 23:21:40 +0900

    moviesのindexアクションを修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
-  #1ページに表示される動画教材を定義
+  # 1ページに表示される動画教材を定義
   PER = 8
 
   def index
-    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page])
+    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(PER)
     page_num = @movies.current_page
     @base_level = (page_num - 1) * PER
   end
