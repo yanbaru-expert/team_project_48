1166957b7000b469cb2bf30f6c84dba08c6e7bd9
mihomatsui <mihomatsui.24@gmail.com>
2021-06-02 10:22:14 +0900

    moviesのコントローラーとビューにページネーションを追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
+  PER = 10
+
   def index
-    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
+    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(PER)
   end
 end
