0d6a95a84c019da7e1b1034249bb84958c85a4da
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-06-05 14:58:10 +0000

    watchコントローラーの重複している部分をメソッド化

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,11 +1,17 @@
 class WatchesController < ApplicationController
+  before_action :set_message
+  
   def create
-    @movie = Movie.find(params[:movie_id])
     current_user.watches.create!(movie_id: @movie.id)
   end
   
   def destroy
+    current_user.watches.find_by(movie_id: @movieid).destroy!
+  end
+  
+  private
+  
+  def set_message
     @movie = Movie.find(params[:movie_id])
-    current_user.watches.find_by(movie_id: @movie.id).destroy!
   end
 end
