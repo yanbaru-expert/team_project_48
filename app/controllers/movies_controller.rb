c173d772a902518d3bbc420d73d34e7b19d0f350
mihomatsui <mihomatsui.24@gmail.com>
2021-06-06 16:08:24 +0900

    movieコントローラーとモデルを修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,10 @@
 class MoviesController < ApplicationController
-  
+  # 1ページに表示される動画教材を定義
+  PER = 8
+
   def index
-    # 1ページに表示される動画教材を定義
-    per = 8
-    # @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(PER)
-    @movies = Movie.recent(per)
+    @movies = Movie.recent.page(params[:page]).per(PER)
     page_num = @movies.current_page
-    @base_level = (page_num - 1) * per
+    @base_level = (page_num - 1) * PER
   end
 end
