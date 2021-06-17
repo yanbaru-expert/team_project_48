a5220d3af7367ea14d3c204eebcf74975b0a43da
mihomatsui <mihomatsui.24@gmail.com>
2021-06-17 20:52:57 +0900

    ルートにusers/guest_sign_inを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root "texts#index"
   resources :texts
   resources :movies
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
