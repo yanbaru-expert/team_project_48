b1a46dcf2f4b087e1e2134734b1fe609479a9379
Ubuntu <ubuntu@ip-172-31-39-202.ap-northeast-1.compute.internal>
2021-07-06 05:18:33 +0000

    /models/movie.rbのscopeの定義を変更

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     @q = Movie.ransack(params[:q])
-    @movies = @q.result.recent.page(params[:page]).per(Movie::PER)
+    @movies = @q.result.recent.includes(:watches).page(params[:page]).per(Movie::PER)
     page_num = @movies.current_page
     @base_level = (page_num - 1) * Movie::PER
   end
