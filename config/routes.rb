de27727231e6cc4376645750798a1d51bbb127c3
itstoasted-0 <snow.w686@gmail.com>
2021-07-16 23:31:17 +0900

    トップページ　コーディング

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     passwords: "users/passwords"
   }
-  root "texts#index"
+  root "pages#top"
   resources :texts
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
