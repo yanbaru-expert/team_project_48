40bdb1a5fccf7a3ee41279b4e84482ac8c28a9a5
mihomatsui <mihomatsui.24@gmail.com>
2021-07-21 20:10:12 +0900

    ボタンを再度設置

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,1 +1,19 @@
 
+// トップへ戻るボタンの実装
+$(function () {
+  var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
