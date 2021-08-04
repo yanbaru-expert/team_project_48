2bb247e39f5186f302a3b3714a0b035a9e1227bd
mihomatsui <mihomatsui.24@gmail.com>
2021-08-05 00:41:33 +0900

    application.jsのコンフリクトを解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap/dist/js/bootstrap"
-<<<<<<< HEAD
 import '@fortawesome/fontawesome-free/js/all';
-=======
-import '@fortawesome/fontawesome-free/js/all'
->>>>>>> origin/develop
 
 Rails.start()
 Turbolinks.start()
